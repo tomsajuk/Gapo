354d9edb7f041189da53c7c40fbe026c3d92aa70
Thomas Saju Koshy <tomsajuk@gmail.com>
2017-08-13 02:06:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 var PythonShell = require('python-shell');
 var file = require('./fileHandling.js');
+var fs = require("fs");
 
 const restService = express();
 
@@ -19,10 +20,12 @@ restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
     
     file.writeToFile(speech);
-
+	var reply = req.body.result.fulfillment.displayText;
+	fs.writeFile('/conversation.txt',speech + ' ' + reply);
 	PythonShell.run('run.py', function (err) {
 	  if (err) throw err;
 	  var data = file.readFromFile();
+	
 		return res.json({
         speech: data,
         displayText: data,
