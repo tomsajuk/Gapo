d80f4d752ff2e4caf023917c0a7fd03616a07824
Thomas Saju Koshy <tomsajuk@gmail.com>
2017-08-12 19:24:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-var PythonShell = require('python-shell'); //new
-var pyshell = new PythonShell('./run.py'); //new
-
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
@@ -17,18 +14,11 @@ restService.use(bodyParser.json());
 // function that gets the text from api.ai. 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
-    //new
-    pyshell.send(speech);
-    pyshell.on('message', function (mes) {
-		console.log(mes);
-		return res.json({
-	        speech: mes,
-	        displayText: speech,
-	        source: 'webhook-heroku'
-	    });
-	});
-
-    //tilll here
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-heroku'
+    });
 });
 
 restService.listen((process.env.PORT || 8000), function() {
