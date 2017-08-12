a6c55010126521143c934461c173315b74e0e503
Thomas Saju Koshy <tomsajuk@gmail.com>
2017-08-12 20:44:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+var run = require('./pytonrun.js');
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
@@ -14,9 +15,18 @@ restService.use(bodyParser.json());
 // function that gets the text from api.ai. 
 restService.post('/echo', function(req, res) {
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+    
+    fs.writeFile('input.txt', speech,  function(err) {
+	   if (err) {
+	      return console.error(err);
+	   }
+	});
+
+	var data = fs.readFileSync('output.txt');
+
     return res.json({
-        speech: speech,
-        displayText: speech,
+        speech: data,
+        displayText: data,
         source: 'webhook-heroku'
     });
 });
